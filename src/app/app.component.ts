fdda4967d907221f468a10ea93be4d7d75a9099b
Yuri Simonov <yura15-96@mail.ru>
2023-07-28 16:03:06 +0300

    3. Вывод данных в шаблон компонента

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-course-2023';
+  title = 'aadasdasd';
+
+  user = {
+    name: 'Yura',
+    age: '27',
+  };
+
+  tooltip = 'Я подсказка для ссылки';
+
+  inlineStyles = {
+    width: '50%',
+    background: 'green',
+  };
+
+  cssClass = 'blue';
+
+  someNumber = '70';
 }
