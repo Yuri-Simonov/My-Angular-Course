4064380c2160138e7d9661cc2de4e705af206a05
Yuri Simonov <yura15-96@mail.ru>
2024-05-19 12:23:38 +0300

    26. Селекторы стилей ng-deep, host и host-context

--- src/app/modules/first/first.component.ts
+++ src/app/modules/first/first.component.ts
@@ -1,9 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrl: './first.component.scss',
-  encapsulation: ViewEncapsulation.None,
 })
 export class FirstComponent {}
