16b3ee5312a9c69613312a41abbb267c185d9007
Yuri Simonov <yura15-96@mail.ru>
2024-11-22 09:16:08 +0300

    39. Обновление до 19-ой версии. Директива @let. Standalone по умолчанию

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChildComponent } from './components/child/child.component';
 import { AsyncPipe } from '@angular/common';
-import { DataService } from './services/data.service';
-import { Observable, of } from 'rxjs';
-import {
-    HttpClient,
-    HttpEvent,
-    HttpHandler,
-    HttpRequest,
-    HttpResponse,
-} from '@angular/common/http';
-import { RandomService } from './services/random.service';
+import { of } from 'rxjs';
 
 @Component({
     selector: 'app-root',
-    standalone: true,
-    imports: [ChildComponent, AsyncPipe],
+    imports: [AsyncPipe],
     templateUrl: './app.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [RandomService],
+    // standalone: false,
 })
 export class AppComponent {
-    users$: Observable<any>;
+    data = {
+        one: {
+            two: {
+                three: {
+                    title: 'Hello world',
+                },
+            },
+        },
+    };
 
-    constructor(
-        private dataService: DataService,
-        private randomService: RandomService
-    ) {
-        this.users$ = this.dataService.getUsers();
-        console.log(this.randomService.getRandomNumber());
-    }
+    asyncData$ = of(this.data);
 
-    // dataService: DataService;
-
-    // constructor() {
-    //     this.dataService = new DataService(new HttpClient(new Handler()));
-    //     this.users$ = this.dataService.getUsers();
-    // }
+    constructor() {}
 }
-
-// class Handler implements HttpHandler {
-//     handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
-//         // Имитация ответа сервера с данными пользователей
-//         const fakeResponse = new HttpResponse({
-//             status: 200,
-//             body: [{ name: 'Вася' }, { name: 'Петя' }],
-//         });
-//         // Возвращаем Observable с нашим фейковым ответом
-//         return of(fakeResponse);
-//     }
-// }
