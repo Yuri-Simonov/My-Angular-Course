3e3898f1745c08ab17acc02db74d111f988f5861
Yuri Simonov <yura15-96@mail.ru>
2025-01-13 14:55:55 +0300

    43. Функция inject. inject vs constructor. HostAttributeToken

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,21 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+    Attribute,
+    ChangeDetectionStrategy,
+    Component,
+    HostAttributeToken,
+    inject,
+    Inject,
+} from '@angular/core';
 import { RandomService } from '../../services/random.service';
-import { ChildDirective } from '../../directives/child.directive';
-import { ParentDirective } from '../../directives/parent.directive';
-import { RedComponent } from '../red/red.component';
-import { GreenComponent } from '../green/green.component';
+import { TOKEN } from '../../shared/tokens/tokens';
 
 @Component({
     selector: 'app-child',
     templateUrl: './child.component.html',
-    styleUrl: './child.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RedComponent, GreenComponent],
-    // providers: [RandomService],
-    // viewProviders: [RandomService],
+    providers: [{ provide: TOKEN, useValue: 123 }],
 })
 export class ChildComponent {
-    constructor(private randomService: RandomService) {
-        console.log('ChildComponent', this.randomService);
+    randomService1 = inject(RandomService, { optional: true, skipSelf: true });
+    token1 = inject(TOKEN);
+    token3: number;
+    name1 = inject(new HostAttributeToken('name'), { optional: true });
+
+    constructor(
+        private randomService2: RandomService,
+        @Inject(TOKEN) private token2: number,
+        @Attribute('name') public name2: string
+    ) {
+        this.token3 = inject(TOKEN);
+
+        console.log('ChildComponent 1', this.randomService1);
+        console.log('ChildComponent 2', this.randomService2);
+
+        console.log('token 1', this.token1);
+        console.log('token 2', this.token2);
+        console.log('token 3', this.token3);
+
+        console.log('name 1', this.name1);
+        console.log('name 2', this.name2);
+    }
+
+    ngOnInit() {
+        // this.token3 = inject(TOKEN);
     }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,19 +1,20 @@
-import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable()
-export class UserService {
-    getUsers() {
-        return of([
-            {
-                name: 'Вася',
-            },
-            {
-                name: 'Маша',
-            },
-            {
-                name: 'Катя',
-            },
-        ]);
+export class BasicService {
+    private httpInject = inject(HttpClient);
+
+    constructor() {}
+    // constructor(private http: HttpClient) {}
+}
+
+@Injectable()
+export class UserService extends BasicService {
+    constructor() {
+        super();
     }
+    // constructor(http: HttpClient) {
+    //     super(http);
+    // }
 }
