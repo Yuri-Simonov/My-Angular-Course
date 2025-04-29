21205dba9788f51d97b60ad2cb38d6f60b46faff
Yuri Simonov <yura15-96@mail.ru>
2025-04-29 09:11:56 +0300

    47. Шаблонные формы в Angular: создание, валидация и отправка на backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,120 @@
+import { NotificationService } from './services/notification.service';
+import { UserService } from './services/user.service';
 import {
     ChangeDetectionStrategy,
-    ChangeDetectorRef,
     Component,
-    inject,
+    DestroyRef,
+    signal,
     ViewChild,
 } from '@angular/core';
 import { FormsModule, NgForm, NgModel } from '@angular/forms';
+import { User } from './interfaces/user';
+import {
+    catchError,
+    combineLatestWith,
+    debounceTime,
+    distinctUntilChanged,
+    EMPTY,
+    switchMap,
+    tap,
+} from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [FormsModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-    username: string = '';
+    initialUser: User = {
+        name: '',
+        lastName: '',
+        email: '',
+        gender: 'male',
+        country: 'ru',
+        comment: '',
+        agree: true,
+    };
+    user: User = structuredClone(this.initialUser);
 
-    @ViewChild('exampleForm') exampleForm: NgForm;
-    @ViewChild('exampleInput') exampleInput: NgModel;
+    enableSubmit = signal(false);
+    emailPending = signal(false);
 
-    private cdr = inject(ChangeDetectorRef);
+    @ViewChild('emailRef') emailRef: NgModel;
+    @ViewChild('userForm') userForm: NgForm;
 
-    ngOnInit() {
-        setTimeout(() => {
-            this.username = 'John Doe';
-            this.cdr.markForCheck();
-        }, 3000);
-    }
-
-    ngDoCheck() {
-        // console.log('this.username', this.username);
-    }
+    constructor(
+        private userService: UserService,
+        private notificationService: NotificationService,
+        private destroyRef: DestroyRef,
+    ) {}
 
     ngAfterViewInit() {
-        this.exampleForm.valueChanges?.subscribe((value) => {
-            console.log('exampleForm value', value);
-        });
-        this.exampleInput.valueChanges?.subscribe((value) => {
-            console.log('exampleInput value', value);
-        });
+        this.emailRef.valueChanges
+            ?.pipe(
+                tap(() => {
+                    this.enableSubmit.set(false);
+                    this.emailPending.set(true);
+                }),
+                debounceTime(1000),
+                switchMap((email) => {
+                    if (this.emailRef.invalid) {
+                        return EMPTY;
+                    }
+
+                    return this.userService.checkEmail(email).pipe(
+                        tap((emailTaken) => {
+                            if (emailTaken) {
+                                this.emailRef.control.setErrors({
+                                    emailTaken: true,
+                                });
+                            } else if (this.emailRef.hasError('emailTaken')) {
+                                delete this.emailRef.control.errors?.[
+                                    'emailTaken'
+                                ];
+                            }
+
+                            this.emailPending.set(false);
+                        }),
+                    );
+                }),
+                catchError(() => {
+                    this.notificationService.error(
+                        'Ошибка при проверке email. Попробуйте позже',
+                    );
+                    return EMPTY;
+                }),
+                combineLatestWith(this.userForm.statusChanges!),
+                takeUntilDestroyed(this.destroyRef),
+            )
+            .subscribe(([emailTaken, formStatus]) => {
+                this.enableSubmit.set(
+                    !emailTaken &&
+                        formStatus === 'VALID' &&
+                        !this.emailPending(),
+                );
+            });
     }
 
-    onSubmit(formValue: NgForm) {
-        console.log('formValue', formValue);
+    checkFieldStatus(field: NgModel) {
+        return field.invalid && (field.dirty || field.touched);
+    }
+
+    onSubmit(userForm: NgForm) {
+        if (!this.enableSubmit()) return;
+
+        this.userService.createUser(userForm.value).subscribe({
+            next: () => {
+                this.notificationService.success('Пользователь успешно создан');
+                userForm.resetForm(this.initialUser);
+            },
+            error: () => {
+                this.notificationService.error(
+                    'Ошибка при создании пользователя. Попробуйте позже',
+                );
+            },
+        });
     }
 }
--- start-code/app/app.config.ts
+++ start-code/app/app.config.ts
@@ -2,16 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideHttpClient(),
-        provideAnimations(), // required animations providers
-        provideToastr(), // Toastr providers
     ],
 };
