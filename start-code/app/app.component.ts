c09203e2a8376e1fd7ddc76c7ddd27b195fddf09
Yuri Simonov <yura15-96@mail.ru>
2025-05-29 09:17:37 +0300

    48. Реактивные формы в Angular. Reactive Forms in Angular

--- start-code/app/app.component.ts
+++ start-code/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
 })
 export class AppComponent {}
